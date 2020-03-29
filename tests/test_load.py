a60aadc44e49b6d174393eca9a38bd6da9656305
vendrox <62812664+vendrox@users.noreply.github.com>
2020-03-28 20:32:32 -0400

    Adding Search Test (#4)
    * Adding Search Test
    
    * Removing Skip

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,3 +1,5 @@
+import pytest
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options 
 import chromedriver_binary 
@@ -16,3 +18,19 @@ def test_simple_load():
 	assert driver.find_element_by_xpath('//*[@id="root"]/h1')
 	driver.quit()
 
+def test_pokemon_search():
+	driver = setup_driver()
+	driver.get("https://egarciasec.github.io/pdex/")
+	input_element = driver.find_element_by_xpath('//*[@id="PokeINFO"]/input')
+	input_element.clear()
+	input_element.send_keys("bulbasaur")
+	search = driver.find_element_by_xpath('//*[@id="PokeINFO"]/button')
+	search.click()
+	for _ in range(0,3):
+		try:
+			pokemon = driver.find_element_by_xpath('//*[@id="root"]/h2')
+			assert pokemon.text == "#1 Bulbasaur"
+			break
+		except:
+			time.sleep(1)
+	driver.quit() 
