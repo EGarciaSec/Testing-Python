d8305d747b1d4e14797e4a236dcfce5c5aa5cc93
EGarciaSec <41349965+EGarciaSec@users.noreply.github.com>
2020-05-17 19:24:09 -0300

    P-06/Migrating to Classes (#5)
    * Migrating to Classes and Removing Chromedriver Path
    
    * removing cache
    
    * Ignore Cache

--- tests/test_load.py
+++ tests/test_load.py
@@ -4,49 +4,50 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options 
 import chromedriver_binary 
 
+class TestLoad:
 
-def setup_driver():
-	chrome_options = webdriver.ChromeOptions()
-	chrome_options.add_argument("--no-sandbox")
-	driver = webdriver.Chrome("C:\\Users\\Usuario\\Testing-Python\\resources\\chromedriver", chrome_options=chrome_options)
-	return driver
+	def setup(self):
+		chrome_options = webdriver.ChromeOptions()
+		chrome_options.add_argument("--no-sandbox")
+		self.driver = webdriver.Chrome(chrome_options=chrome_options)
 
-def test_simple_load():
-	driver = setup_driver()
-	driver.get("https://egarciasec.github.io/pdex/")
-	assert driver.find_element_by_xpath('//*[@id="root"]/h1')
-	driver.quit()
+	def teardown(self):
+		self.driver.quit()
 
-def test_pokemon_search():
-	driver = setup_driver()
-	driver.get("https://egarciasec.github.io/pdex/")
-	input_element = driver.find_element_by_xpath('//*[@id="PokeINFO"]/input')
-	input_element.clear()
-	input_element.send_keys("bulbasaur")
-	search = driver.find_element_by_xpath('//*[@id="PokeINFO"]/button')
-	search.click()
-	for _ in range(0,3):
-		try:
-			pokemon = driver.find_element_by_xpath('//*[@id="root"]/h2')
-			assert pokemon.text == "#1 Bulbasaur"
-			break
-		except:
-			time.sleep(1)
-	driver.quit() 
 
-def test_pokemon_image():
-	driver = setup_driver()
-	driver.get("https://egarciasec.github.io/pdex/")
-	input_element = driver.find_element_by_xpath('//*[@id="PokeINFO"]/input')
-	input_element.clear()
-	input_element.send_keys("bulbasaur")
-	search = driver.find_element_by_xpath('//*[@id="PokeINFO"]/button')
-	search.click()
-	for _ in range(0,3):
-		try:
-			image_element = driver.find_element_by_xpath('//*[@id="root"]/img')
-			assert image.size['height'] > 0
-			break
-		except:
-			time.sleep(1)
-	driver.quit() 
+	def test_simple_load(self):
+		driver = self.driver
+		driver.get("https://egarciasec.github.io/pdex/")
+		assert driver.find_element_by_xpath('//*[@id="root"]/h1')
+
+	def test_pokemon_search(self):
+		driver = self.driver
+		driver.get("https://egarciasec.github.io/pdex/")
+		input_element = driver.find_element_by_xpath('//*[@id="PokeINFO"]/input')
+		input_element.clear()
+		input_element.send_keys("bulbasaur")
+		search = driver.find_element_by_xpath('//*[@id="PokeINFO"]/button')
+		search.click()
+		for _ in range(0,3):
+			try:
+				pokemon = driver.find_element_by_xpath('//*[@id="root"]/h2')
+				assert pokemon.text == "#1 Bulbasaur"
+				break
+			except:
+				time.sleep(1)
+
+	def test_pokemon_image(self):
+		driver = self.driver
+		driver.get("https://egarciasec.github.io/pdex/")
+		input_element = driver.find_element_by_xpath('//*[@id="PokeINFO"]/input')
+		input_element.clear()
+		input_element.send_keys("bulbasaur")
+		search = driver.find_element_by_xpath('//*[@id="PokeINFO"]/button')
+		search.click()
+		for _ in range(0,3):
+			try:
+				image_element = driver.find_element_by_xpath('//*[@id="root"]/img')
+				assert image_element.size['height'] > 0
+				break
+			except:
+				time.sleep(1)
